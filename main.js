dc1d5437f19ef9f4fe4e899fd0a3f14283d90701
Abhay Zala <zala.abhay@gmail.com>
2021-10-06 00:09:52 -0400

    mild_1 and mild_1 tests

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-import {sumToString} from "./src/mild/mild_1.js"
+import { sumToString, getIncreasingArray, maxAndMin, countArray } from "./src/mild/mild_1.js"
+import { equal } from 'assert';
 
-let x = sumToString(3, 4);
+equal(sumToString(3, 4), "3 + 4 = 7");
 
-console.log(x);
+let arr1 = getIncreasingArray(3, 7);
+let arr2 = [3, 4, 5, 6, 7];
+
+for (let i=0;i<arr2.length;i++) {
+	equal(arr1[i], arr2[i]);
+}
+
+let maxMin = maxAndMin([1, 2, 3, 4, 5]);
+equal(maxMin.min, 1);
+equal(maxMin.max, 5);
+
+
+let counts = countArray(["hello","hello", 2, 2, 1, "bean"]);
+equal(counts['2'], 2);
+equal(counts.hello, 2);
+equal(counts['1'], 1);
+equal(counts.bean, 1);
+
+
+
+
+
+
+
+console.log('\u001b[' + 32 + 'm' + "Passed all checks!" + '\u001b[0m');
